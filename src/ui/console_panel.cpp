a91f30de334c941cdc49c9a7f80fc99b3b00653f
Matthew Zegar <matthewzegar@gmail.com>
2024-01-14 00:26:18 -0800

    Console panel input text

--- src/ui/console_panel.cpp
+++ src/ui/console_panel.cpp
@@ -1,14 +1,62 @@
 #include "console_panel.hpp"
 
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-
 ConsolePanel::ConsolePanel() {}
 
 void ConsolePanel::Render()
 {
+    ImGui::ShowDemoWindow();
+
     ImGui::Begin("Console");
 
+    // Reserve enough left-over height for 1 separator + 1 input text
+    const float footer_height_to_reserve = ImGui::GetStyle().ItemSpacing.y + ImGui::GetFrameHeightWithSpacing();
+    ImGui::BeginChild("ScrollingRegion", ImVec2(0, -footer_height_to_reserve), false, ImGuiWindowFlags_HorizontalScrollbar);
+    if (ImGui::BeginPopupContextWindow())
+    {
+        // if (ImGui::Selectable("Clear"))
+        //     ClearLog();
+        ImGui::EndPopup();
+    }
+
+    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(4, 1)); // Tighten spacing
+    for (const auto &command : _previousCommands)
+    {
+        ImVec4 color = ImVec4(1.0f, 0.8f, 0.6f, 1.0f);
+        ImGui::TextUnformatted(command.c_str());
+    }
+
+    if (_scrollToBottom || (ImGui::GetScrollY() >= ImGui::GetScrollMaxY()))
+        ImGui::SetScrollHereY(1.0f);
+    _scrollToBottom = false;
+
+    ImGui::PopStyleVar();
+    ImGui::EndChild();
+    ImGui::Separator();
+
+    // Command-line
+    ImGui::PushItemWidth(-1);
+    bool reclaim_focus = false;
+    ImGuiInputTextFlags input_text_flags = ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_CallbackCompletion | ImGuiInputTextFlags_CallbackHistory;
+    if (ImGui::InputText("##label", &_input, input_text_flags))
+        ExecuteCommand(_input);
+
+    // Auto-focus on window apparition
+    ImGui::SetItemDefaultFocus();
+    if (reclaim_focus)
+        ImGui::SetKeyboardFocusHere(-1); // Auto focus previous widget
+
     ImGui::End();
+}
+
+void ConsolePanel::ExecuteCommand(const std::string &command)
+{
+    AddLog(command);
+
+    _input = "";
+    _scrollToBottom = true;
+}
+
+void ConsolePanel::AddLog(const std::string &log)
+{
+    _previousCommands.push_back(log);
 }
--- src/ui/console_panel.hpp
+++ src/ui/console_panel.hpp
@@ -1,9 +1,29 @@
 #pragma once
 
+#include <string>
+
+#include "imgui.h"
+#include "imgui_stdlib.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 class ConsolePanel
 {
 public:
     ConsolePanel();
 
     void Render();
+
+private:
+    std::string _input;
+    ImVector<std::string> _previousCommands;
+
+    ImVector<const char *> Commands;
+    ImVector<char *> History;
+    int HistoryPos; // -1: new line, 0..History.Size-1 browsing history.
+    ImGuiTextFilter Filter;
+    bool _scrollToBottom;
+
+    void ExecuteCommand(const std::string &command);
+    void AddLog(const std::string &log);
 };
