5241ea3dd584a476c5a9963ed9f9ce99dd4fb423
Matthew Zegar <matthewzegar@gmail.com>
2024-02-05 22:02:10 -0500

    Remove extra glClear()

--- src/render/opengl_context.cpp
+++ src/render/opengl_context.cpp
@@ -8,7 +8,6 @@ OpenGLContext::OpenGLContext(Window *window) : _window(window)
 
 void OpenGLContext::PreRender()
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 void OpenGLContext::PostRender()
